d6ac7142a7f910144804d4bde5879bf19f53533b
lai dingqing <37505218@qq.com>
2023-01-30 09:53:54 +0800

    project clear up

--- src/native-test/java/com/foyo/bi/GreetingResourceIT.java
+++
@@ -1,8 +0,0 @@
-package com.foyo.bi;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class GreetingResourceIT extends GreetingResourceTest {
-    // Execute the same tests but in packaged mode.
-}
